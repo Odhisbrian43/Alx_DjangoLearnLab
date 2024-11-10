73b47e2bd8290aa46680e84a27c409d1fb1f4200
odhisbrian43 <odhisbrian43@gmail.com>
2024-11-10 23:12:16 +0300

    django advanced

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -7,3 +7,5 @@ Library.objects.get(books)
 Library.objects.get(librarian)
 
 Library.objects.get(name=library_name), books.all()
+
+Author.objects.get(name=author_name), objects.filter(author=author)
