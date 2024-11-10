4b86a0ede10c6c0b45ec38b5e23aef11a0ef22c8
odhisbrian43 <odhisbrian43@gmail.com>
2024-11-10 23:31:10 +0300

    django advanced

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -46,6 +46,6 @@ def logout_view(request):
     logout(request)
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    template_name = 'relationship_app/register.html'
