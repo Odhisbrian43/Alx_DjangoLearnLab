94ad09a278b2182eb07956237a12e99856926acf
odhisbrian43 <odhisbrian43@gmail.com>
2024-11-10 22:45:53 -0800

    django advanced

---
+++ django-models/relationship_app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, primary_key=True)
+
+user = User.objects.create_user('john', 'john@example.com', 'password123')
+
+user = User.objects.get(username='john')
