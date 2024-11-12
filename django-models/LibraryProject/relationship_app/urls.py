b5bc958261073e55abc807f87cbc0be82237cd03
odhisbrian43 <odhisbrian43@gmail.com>
2024-11-12 18:29:39 +0300

    django advanced

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('', view = views.register, name = 'register')
     path('', LogoutView.as_view(template_name="logout"))
     path('', LoginView.as_view(template_name='login'))
+    path('add_book/',)
+    path('edit_book/',)
+    path('delete_book', )
 ]
