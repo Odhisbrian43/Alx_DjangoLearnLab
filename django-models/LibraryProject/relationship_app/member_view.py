ac5e093b6f5901fcefbe1977510f7554c057e730
odhisbrian43 <odhisbrian43@gmail.com>
2024-11-12 14:28:41 +0300

    django advanced

--- django-models/LibraryProject/relationship_app/member_view.py
+++ django-models/LibraryProject/relationship_app/member_view.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import permission_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
+#Decorator function
+
 @method_decorator(permission_required, name="dispatch")
 class ProtectedView(TemplateView):
     template_name = "member.html"
