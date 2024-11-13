6aefcc075a485da6f6961aba6368477bf189afb5
odhisbrian43 <odhisbrian43@gmail.com>
2024-11-13 12:12:51 +0300

    advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 class Book(models.Model):
     title = models.CharField(max_length = 200)
@@ -7,3 +8,7 @@ class Book(models.Model):
 
     def __str__(self):
         return "f{self.title} {self.author} {self.publication_year}"
+    
+class Customuser(AbstractUser):
+        date_of_birth = models.DateField()
+        profile_photo = models.ImageField()
