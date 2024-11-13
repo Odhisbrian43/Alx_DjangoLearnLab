e20f0cce16db28eb3a38413d4b11547cf141ab31
odhisbrian43 <odhisbrian43@gmail.com>
2024-11-13 12:20:19 +0300

    advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -9,6 +9,6 @@ class Book(models.Model):
     def __str__(self):
         return "f{self.title} {self.author} {self.publication_year}"
     
-class Customuser(AbstractUser):
+class CustomUser(AbstractUser):
         date_of_birth = models.DateField()
         profile_photo = models.ImageField()
