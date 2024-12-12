3a00a7c851bdf075bbb86d637644f2a01c81827b
odhisbrian43 <odhisbrian43@gmail.com>
2024-12-12 20:30:34 +0300

    social media api

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import CustomUser
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    actor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    verb = models.CharField(max_length=20)
+    target = GenericForeignKey(verb)
+    timestamp = models.TimeField(auto_now=True)
