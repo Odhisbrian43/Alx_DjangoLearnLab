05e031ba418dda6c79fb369385e11c9943878f9f
odhisbrian43 <odhisbrian43@gmail.com>
2024-12-12 20:50:20 +0300

    social media api

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -145,3 +145,29 @@ REST_FRAMEWORK = {
        'rest_framework.authentication.TokenAuthentication',
    ),
 }
+
+## X-XSS-Protection
+
+SECURE_BROWSER_XSS_FILTER = True
+
+## X-Frame-Options
+
+X_FRAME_OPTIONS = 'DENY'
+
+#X-Content-Type-Options
+SECURE_CONTENT_TYPE_NOSNIFF = True
+## Strict-Transport-Security
+SECURE_HSTS_SECONDS = 15768000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+
+SECURE_SSL_REDIRECT = True 
+
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'Strict'
